566ac3beb09259199dc99d1b1286ddc3d1671c9f
victorious-firmware-2064 <alanna.nuala@gmail.com>
2019-10-12 21:39:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ function theBeatlesPlay(players, instruments) {
 }
 
 
-let facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
 
 function johnLennonFacts(arr = facts) {
+  let facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
     let newFacts = [];
     let i = 0;
     while (i < facts.length) {
