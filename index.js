40b76b7f0d9ecc0f124decae5900c5fa769cfb80
victorious-firmware-2064 <alanna.nuala@gmail.com>
2019-10-12 21:35:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function theBeatlesPlay(players, instruments) {
 }
 
 
-function johnLennonFacts() {
+function johnLennonFacts(arr = facts) {
     let facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
     let newFacts = [];
     let i = 0;
